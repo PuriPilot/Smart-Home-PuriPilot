8bc24bd95b799a67690d2703e2d77b99daa6d970
nameismain <idid02@naver.com>
2025-12-05 14:31:14 +0900

    fix: ReferenceError

--- example/js/example.js
+++ example/js/example.js
@@ -520,7 +520,19 @@ var mainControls = function(blueprint3d) {
 
 $(document).ready(function() {
 
-  var API_BASE = 'http://localhost:3001/api';
+  var API_BASE = (function () {
+    var search = new URLSearchParams(window.location.search);
+    var override = search.get('apiBase');
+    var configured = window.API_BASE || window.__API_BASE__ || override;
+    if (configured) return configured.replace(/\/$/, '');
+
+    var isLocal = /localhost|127\.0\.0\.1/.test(window.location.hostname);
+    if (isLocal) {
+      return 'http://localhost:3001/api';
+    }
+
+    return window.location.origin.replace(/\/$/, '') + '/api';
+  })();
   var DEFAULT_SCENE = '{"floorplan":{"corners":{"f90da5e3-9e0e-eba7-173d-eb0b071e838e":{"x":204.85099999999989,"y":289.052},"da026c08-d76a-a944-8e7b-096b752da9ed":{"x":672.2109999999999,"y":289.052},"4e3d65cb-54c0-0681-28bf-bddcc7bdb571":{"x":672.2109999999999,"y":-178.308},"71d4f128-ae80-3d58-9bd2-711c6ce6cdf2":{"x":204.85099999999989,"y":-178.308}},"walls":[{"corner1":"71d4f128-ae80-3d58-9bd2-711c6ce6cdf2","corner2":"f90da5e3-9e0e-eba7-173d-eb0b071e838e","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"f90da5e3-9e0e-eba7-173d-eb0b071e838e","corner2":"da026c08-d76a-a944-8e7b-096b752da9ed","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"da026c08-d76a-a944-8e7b-096b752da9ed","corner2":"4e3d65cb-54c0-0681-28bf-bddcc7bdb571","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"4e3d65cb-54c0-0681-28bf-bddcc7bdb571","corner2":"71d4f128-ae80-3d58-9bd2-711c6ce6cdf2","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}}],"wallTextures":[],"floorTextures":{},"newFloorTextures":{}},"items":[]}';
   var currentFloorplanId = null;
   var autosaveDirty = false;
